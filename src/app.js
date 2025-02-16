7523bf917b5ae700f0e9bee0144e49b5b13e1be3
Otavio Emanoel <otavioemanuel6b@gmail.com>
2025-02-16 00:03:44 -0300

    Criação dos cards

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const produtos = require('./products/products_list')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -9,39 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-const produtos = [
-    {
-        nome: "iPhone 15 128gb",
-        marca: "Apple",
-        preco: 8000,
-        imagem: "https://a-static.mlcdn.com.br/1500x1500/iphone-15-apple-128gb-tela-de-6-1-camera-dupla-de-48mp-azul-mtp43br-a/kabum/511384/bdac051104250b55c79d9545a3ca28f3.jpeg"
-    },
-    {
-        nome: "Moto G52",
-        marca: "Motorola",
-        preco: 1200,
-        imagem: "https://cdn.avaliado.com.br/media/p/celular-motorola-moto-g52-branco-128gb-tela-66-4gb-ram-camera-tripla_7WMGLE6.jpg"
-    },
-    {
-        nome: "Redmi Note 13",
-        marca: "Xiaomi",
-        preco: 1100,
-        imagem: "https://www.armazemparaiba.com.br/ccstore/v1/images/?source=/file/v7543687176933702589/products/0100131982930006.11a1d33ad10be24be98dc84cf3f4c2293103c450.gif&height=100&width=100"
-    },
-    {
-        nome: "Poco F6 Pro",
-        marca: "Xiaomi",
-        preco: 3067,
-        imagem: "https://www.armazemparaiba.com.br/ccstore/v1/images/?source=/file/v7543687176933702589/products/0100131982930006.11a1d33ad10be24be98dc84cf3f4c2293103c450.gif&height=100&width=100"
-    },
-    {
-        nome: "Galaxy A55",
-        marca: "Samsung",
-        preco: 1800,
-        imagem: "https://martinelloeletrodomesticos.fbitsstatic.net/img/p/smartphone-samsung-galaxy-a55-5g-azul-escuro-256gb-8gb-ram-cam-tripla-selfie-32mp-79629/266221.jpg?w=482&h=482&v=no-change&qs=ignore"
-    }
-    
-]
+
 
 app.get('/', (req, res) => {
     res.render('index', {
---
+++ src/products/products_list.js
@@ -0,0 +1,94 @@
+const produtos = [
+    {
+        nome: "iPhone 15 128gb",
+        marca: "Apple",
+        preco: 8000,
+        imagem: "https://a-static.mlcdn.com.br/1500x1500/iphone-15-apple-128gb-tela-de-6-1-camera-dupla-de-48mp-azul-mtp43br-a/kabum/511384/bdac051104250b55c79d9545a3ca28f3.jpeg"
+    },
+    {
+        nome: "Moto G52",
+        marca: "Motorola",
+        preco: 1200,
+        imagem: "https://cdn.avaliado.com.br/media/p/celular-motorola-moto-g52-branco-128gb-tela-66-4gb-ram-camera-tripla_7WMGLE6.jpg"
+    },
+    {
+        nome: "Redmi Note 13",
+        marca: "Xiaomi",
+        preco: 1100,
+        imagem: "https://www.armazemparaiba.com.br/ccstore/v1/images/?source=/file/v7543687176933702589/products/0100131982930006.11a1d33ad10be24be98dc84cf3f4c2293103c450.gif&height=100&width=100"
+    },
+    {
+        nome: "Poco F6 Pro",
+        marca: "Xiaomi",
+        preco: 3067,
+        imagem: "https://horizonplay.fbitsstatic.net/img/p/smartphone-xiaomi-poco-f6-5g-256gb-com-8ram-titanio-dourado-versao-global-150503/337097.jpg?w=420&h=420&v=no-value"
+    },
+    {
+        nome: "Galaxy A55",
+        marca: "Samsung",
+        preco: 1800,
+        imagem: "https://martinelloeletrodomesticos.fbitsstatic.net/img/p/smartphone-samsung-galaxy-a55-5g-azul-escuro-256gb-8gb-ram-cam-tripla-selfie-32mp-79629/266221.jpg?w=482&h=482&v=no-change&qs=ignore"
+    },
+    {
+        nome: "iPhone 14 Pro Max",
+        marca: "Apple",
+        preco: 9000,
+        imagem: "https://www.iplacecorp.com.br/ccstore/v1/images/?source=/file/v1042946294058209932/products/222278.00.638479089781047514-apple-iphone-14-pro-max-512gb-prateado-mqah3be.jpg&height=475&width=475"
+    },
+    {
+        nome: "Galaxy S24 Ultra",
+        marca: "Samsung",
+        preco: 7500,
+        imagem: "https://samsungbrshop.vtexassets.com/arquivos/ids/228347/1---_S767B~1.png?v=638411051051530000"
+    },
+    {
+        nome: "Edge 40",
+        marca: "Motorola",
+        preco: 2500,
+        imagem: "https://m.media-amazon.com/images/I/61IlIDPO1DL.jpg"
+    },
+    {
+        nome: "Xiaomi 13 Pro",
+        marca: "Xiaomi",
+        preco: 5000,
+        imagem: "https://down-br.img.susercontent.com/file/sg-11134201-7rd6c-lvutc196aeie86"
+    },
+    {
+        nome: "ROG Phone 7",
+        marca: "Asus",
+        preco: 6500,
+        imagem: "https://images.kabum.com.br/produtos/fotos/sync_mirakl/475684/Smartphone-Asus-Rog-Phone-7-16GB-RAM-512GB-5G-Octa-Core-Snapdragon-8-Gen2-C-mtripla-self-32mp-Tela-6-78-Polegadas-Branco_1711492086_gg.jpg"
+    },
+    {
+        nome: "GT 3",
+        marca: "Realme",
+        preco: 3200,
+        imagem: "https://a-static.mlcdn.com.br/1500x1500/celular-realme-gt3-dual-sim-1-tb-16-gb-ram-5g-nfc-240w-pulse-white/gabriellavendas/21d4b412de7811ef9c9e4201ac18503c/94757b4180f8cd64e9444db725b6f67d.jpeg"
+    },
+    {
+        nome: "Pixel 8 Pro",
+        marca: "Google",
+        preco: 6500,
+        imagem: "https://images.tcdn.com.br/img/img_prod/625110/180_google_pixel_8_pro_512gb_porcelain_5471_1_c1b7cd69f43877b2d8f31a8ea057ee96.jpg"
+    },
+    {
+        nome: "Nokia XR21",
+        marca: "Nokia",
+        preco: 2300,
+        imagem: "https://t2.tudocdn.net/667076?w=1000&fit=clip"
+    },
+    {
+        nome: "OnePlus 11",
+        marca: "OnePlus",
+        preco: 4500,
+        imagem: "https://oasis.opstatics.com/content/dam/oasis/page/2023/na/oneplus-11/specs/green-img.png"
+    },
+    {
+        nome: "Xperia 1 V",
+        marca: "Sony",
+        preco: 8000,
+        imagem: "https://cdn.awsli.com.br/800x800/1271/1271561/produto/274135612/smartphone-sony-xperia-1-v-256gb--1--wd52o29hjw.jpg"
+    }
+]
+
+module.exports = produtos
